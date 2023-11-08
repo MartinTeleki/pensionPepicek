30cdaebce9998700be8d0976ac1f630761883272
MartinTeleki <Martinteleki@seznam.cz>
2023-11-08 01:12:22 +0100

    galerie added

--- src/Ui/Galerie.jsx
+++ src/Ui/Galerie.jsx
@@ -1,23 +1,84 @@
 import styles from "./Galerie.module.css";
 import { Link } from "react-router-dom";
+import pokojnadkolarnou from "../img/pokojnadkolarnou.webp";
+import pokojnadkrcmou from "../img/pokojnadkrcmou.webp";
+import pokojslunce from "../img/pokojslunce.webp";
+import pokojpohoda from "../img/pokojpohoda.webp";
+import pokojjezero from "../img/pokojjezero.webp";
+import okoli1 from "../img/okoli1.webp";
+import apartman1 from "../img/apartman1.webp";
+import bar5 from "../img/bar5.webp";
 
 function Galerie() {
   return (
     <div className={styles.layout}>
       <ul className={styles.apartmanLinks}>
-        <Link to="/galerie/areal"> Galerie Areal</Link>
+        <div className={styles.apartmanContainer}>
+          <div className={styles.linkContainer}>
+            <Link to="/galerie/nadkrcmou">Galerie apartmán Nad Krčmou</Link>
+          </div>
 
-        <Link to="/galerie/bar"> Galerie Bar</Link>
+          <img src={pokojnadkolarnou} alt="pokoj Nad Krčmou" />
+        </div>
+        <div className={styles.apartmanContainer}>
+          <div className={styles.linkContainer}>
+            <Link to="/galerie/nadkolarnou">Galerie apartmán Nad Kolárnou</Link>
+          </div>
+          <div>
+            <img src={pokojnadkrcmou} alt="pokoj Nad Kolárnou" />
+          </div>
+        </div>
+        <div className={styles.apartmanContainer}>
+          <div className={styles.linkContainer}>
+            <Link to="/galerie/slunce">Galerie apartmán Slunce</Link>
+          </div>
+          <div>
+            <img src={pokojslunce} alt="pokoj Slunce" />
+          </div>
+        </div>
+        <div className={styles.apartmanContainer}>
+          <div className={styles.linkContainer}>
+            <Link to="/galerie/pohoda">Galerie apartmán Pohoda</Link>
+          </div>
+          <div>
+            <img src={pokojpohoda} alt="pokoj Pohoda" />
+          </div>
+        </div>
+        <div className={styles.apartmanContainer}>
+          <div className={styles.linkContainer}>
+            <Link to="/galerie/jezero">Galerie apartmán Jezero</Link>
+          </div>
+          <div>
+            <img src={pokojjezero} alt="pokoj Jezero" />
+          </div>
+        </div>
 
-        <Link to="/galerie/jezero"> Galerie Jezero</Link>
+        <div className={styles.apartmanContainer}>
+          <div className={styles.linkContainer}>
+            <Link to="/galerie/okoli">Galerie okolí penzionu</Link>
+          </div>
+          <div>
+            <img src={okoli1} alt="pokoj Jezero" />
+          </div>
+        </div>
 
-        <Link to="/galerie/nadkolarnou"> Galerie Kolárna</Link>
+        <div className={styles.apartmanContainer}>
+          <div className={styles.linkContainer}>
+            <Link to="/galerie/bar">Galerie Baru</Link>
+          </div>
+          <div>
+            <img src={bar5} alt="pokoj Jezero" />
+          </div>
+        </div>
 
-        <Link to="/galerie/nadkrcmou"> Galerie Krcma</Link>
-
-        <Link to="/galerie/okoli"> Galerie Okolí</Link>
-
-        <Link to="/galerie/slunce"> Galerie Slunce</Link>
+        <div className={styles.apartmanContainer}>
+          <div className={styles.linkContainer}>
+            <Link to="/galerie/areal">Galerie areál</Link>
+          </div>
+          <div>
+            <img src={apartman1} alt="areál penzionu Pepíček" />
+          </div>
+        </div>
       </ul>
     </div>
   );
--- src/Ui/galerieApartman/GaleriePohoda.jsx
+++ src/Ui/galerieApartman/GaleriePohoda.jsx
@@ -1,5 +1,9 @@
 import ButtonInfo from "./ButtonInfo";
 import styles from "./GalerieApartman.module.css";
+import pohoda1 from "../../img/pohoda1.webp";
+import pohoda2 from "../../img/pohoda2.webp";
+import pohoda3 from "../../img/pohoda3.webp";
+import pohoda4 from "../../img/pohoda4.webp";
 import { useNavigate } from "react-router-dom";
 
 function GaleriePohoda() {
@@ -8,7 +12,16 @@ function GaleriePohoda() {
     <div className={styles.layout}>
       <section className={styles.apartmanSection}>
         <div>
-          <img src="" alt="" />
+          <img src={pohoda1} alt="obyvák s televizí" />
+        </div>
+        <div>
+          <img src={pohoda2} alt="schodiště s kuchyní" />
+        </div>
+        <div>
+          <img src={pohoda3} alt="kuchyně" />
+        </div>
+        <div>
+          <img src={pohoda4} alt="sprchový kout" />
         </div>
 
         <div className={styles.button}>
